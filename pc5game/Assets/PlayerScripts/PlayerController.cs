6915bb2f132870a7f82254893287b84e0f7e3146
Dennis Truong <dennisnhatsontruong@gmail.com>
2019-11-14 18:53:31 -0800

    basic movement of sprite
    Created example white circle sprite. Created simple WASD movement with speed as a serialized field

---
+++ pc5game/Assets/PlayerScripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector2 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+        move();
+    }
+
+    public void move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void getInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+    }
+}
