b947cc790895547578e6ce085bdec1deb696d019
m-00 <57302916+m-00@users.noreply.github.com>
2020-01-08 00:02:26 -0800

    Create attack ScriptableObjects and edit Attacker
    Edit Attacker script to accept ScriptableObjects
    with attack data. Each ScriptableObject asset
    stores info for a single attack (audio, distance
    etc). Each attack has its own Animator Override
    Controller.

--- pc5game/Assets/Scripts/Regular Class/KeyMap.cs
+++ pc5game/Assets/Scripts/Regular Class/KeyMap.cs
@@ -9,6 +9,7 @@ public static class KeyMap
     public static KeyCode Examine { get; set; } = KeyCode.F;
     public static KeyCode NextSentence { get; set; } = KeyCode.F;
     public static KeyCode Attack { get; set; } = KeyCode.Space;
+    public static KeyCode AttackSwitch { get; set; } = KeyCode.LeftShift;
 
     public static KeyCode MoveUp { get; set; } = KeyCode.W;
     public static KeyCode MoveLeft { get; set; } = KeyCode.A;
---
+++ pc5game/Assets/Scripts/ScriptableObjects/AttackData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AttackData", menuName = "ScriptableObjects/Attack", order = 35)]
+public class AttackData : ScriptableObject
+{
+    public string Name;
+    public int Damage;
+    public AudioClip Audio;
+    public float Area;
+    public float Distance;
+    public AnimatorOverrideController animController;
+}
