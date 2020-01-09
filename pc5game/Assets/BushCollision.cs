e1616b67ef7f21f767873a3ac5070f26f146dd0e
saba <x3y2b@ugrad.cs.ubc.ca>
2020-01-08 22:33:08 -0800

    added particle effect for bush collision

---
+++ pc5game/Assets/BushCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushCollision : MonoBehaviour
+{
+    public Transform sparkle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(stopSparkles());
+    }
+
+    IEnumerator stopSparkles()
+    {
+        yield return new WaitForSeconds (.4f);
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
