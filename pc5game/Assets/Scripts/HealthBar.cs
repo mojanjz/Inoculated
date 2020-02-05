fc5461233fadeeebd4fd7b9e5e1cfae916509576
saba <x3y2b@ugrad.cs.ubc.ca>
2020-02-04 19:07:49 -0800

    health bar added

---
+++ pc5game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private Transform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+       bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar = transform.Find("Bar");
+        bar.localScale = new Vector3(sizeNormalized*20, 4f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
