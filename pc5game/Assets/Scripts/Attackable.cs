8d0a35ecfd41ddaff39b0e66d0dad361bfbc10e5
Dennis Truong <dennisnhatsontruong@gmail.com>
2020-02-05 11:42:16 -0800

    added enemy stun timer

--- pc5game/Assets/Scripts/Attackable.cs
+++ pc5game/Assets/Scripts/Attackable.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Attackable : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int stunTimer;
+    private string state;
+    private int health;
     
     void Start()
     {
-        
+        health = maxHealth;
+        state = "alive";
     }
 
     // Update is called once per frame
@@ -21,23 +26,51 @@ public class Attackable : MonoBehaviour
 
     public void OnAttack(int damage)
     {
-        health -= damage;
-        if (health <= 0)
+        if (state != "stunned") 
         {
-            Die();
+            health -= damage;
+            if (health <= 0)
+            {
+                if (gameObject.tag == "Enemy")
+                {
+                    Stun(stunTimer);
+                }
+                else
+                {
+                    Die();
+                }
+            }
+            else
+            {
+                Debug.Log(gameObject.name + " has been hit, now has " + maxHealth + " health left");
+            }
         }
-        else {
-            Debug.Log(gameObject.name + " has been hit, now has " + health + " health left" );
-        }
+        
         
     }
 
-
+    private void Stun(int time) 
+    {
+        state = "stunned";
+        StartCoroutine(disableMovementandReEnable(time));
+        
+    }
 
     public void Die()
     {
         Debug.Log("SNAPPED OUT OF EXISTENCE.");
         Destroy(gameObject);
     }
+
+    private IEnumerator disableMovementandReEnable(int time)
+    {
+        (gameObject.GetComponent("EnemyMovement") as MonoBehaviour).enabled = false;
+        yield return new WaitForSeconds(time);
+        health = maxHealth;
+        state = "alive";
+        gameObject.GetComponent<EnemyMovement>().findInitialTargets();
+        (gameObject.GetComponent("EnemyMovement") as MonoBehaviour).enabled = true;
+    }
+
 
 }
--- pc5game/Assets/Scripts/EnemyMovement.cs
+++ pc5game/Assets/Scripts/EnemyMovement.cs
@@ -7,7 +7,8 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Transform target;
     [SerializeField] private Transform nonTarget;
-    [SerializeField] private float aquisitionTime;
+    [SerializeField] private float aquisitionTime;  //time required to change target
+    [SerializeField] private float minDistanceRadius;
     [SerializeField] float count;
     
 
@@ -15,16 +16,32 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        //nonTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        count = 0;
+        findInitialTargets();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         targetAquisition();
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        moveTowardsTarget();
+    }
+
+    public void findInitialTargets()
+    {
+        GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
+        float distance0 = Vector2.Distance(transform.position, playerList[0].transform.position);
+        float distance1 = Vector2.Distance(transform.position, playerList[1].transform.position);
+        if (distance0 <= distance1)
+        {
+            target = playerList[0].transform;
+            nonTarget = playerList[1].transform;
+        }
+        else
+        {
+            target = playerList[1].transform;
+            nonTarget = playerList[0].transform;
+        }
     }
 
 
@@ -44,17 +61,24 @@ public class EnemyMovement : MonoBehaviour
             count = 0;
         }
 
+        //Switches target player
         if (count >= aquisitionTime)
         {
             temp = target;
             target = nonTarget;
             nonTarget = temp;
         }
+    }
 
+    private void moveTowardsTarget()
+    {
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
 
+        if (distanceToTarget > minDistanceRadius)
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
     }
 
-   
+
 
 }
