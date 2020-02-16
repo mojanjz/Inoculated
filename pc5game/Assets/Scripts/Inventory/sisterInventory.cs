bbf05d5759eec7be1377e1853e5fe2fff1b3a9a9
Jamalzadeh, Mojan <mojan.jamalzadeh@philips.com>
2020-02-16 14:48:43 -0800

    started on the sister's inventory

---
+++ pc5game/Assets/Scripts/Inventory/sisterInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sisterInventory : MonoBehaviour
+{
+    [SerializeField] private InventoryKeyMap inventoryKeyMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+    }
+
+    private void getInput()
+    {
+
+        if (Input.GetKey(inventoryKeyMap.RightCell))
+        {
+
+        }
+        if (Input.GetKey(inventoryKeyMap.LeftCell))
+        {
+
+        }
+        if (Input.GetKey(inventoryKeyMap.UseObject))
+        {
+            Debug.Log("use the object");
+        }
+
+    }
+}
