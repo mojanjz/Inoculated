a396e0d15b1250e6f0aab2a2d0277d188ef9e6ae
Jamalzadeh, Mojan <mojan.jamalzadeh@philips.com>
2020-02-16 14:17:13 -0800

    drop functionality for the brother works

--- pc5game/Assets/Scripts/Pickup.cs
+++ pc5game/Assets/Scripts/Pickup.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 // script to be added to items that the player can pick up
 public class Pickup : MonoBehaviour
 {
-    private Inventory inventory;
+    public Inventory inventory;
     public GameObject itemButton;
     [SerializeField] private Canvas canvas;
 
@@ -18,7 +18,7 @@ public class Pickup : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("I collided with the object!");
+        //Debug.Log("I collided with the object!");
        if (other.CompareTag("Player"))
         {
             // Get the inventory of the specific player
@@ -30,14 +30,10 @@ public class Pickup : MonoBehaviour
                 if(inventory.isFull[i] == false)
                 { // let's fill it
                     inventory.isFull[i] = true;
-
-                    //Instantiate(itemButton,inventory.slots[i].transform,false); //instantiates it at the inventory slot
                     Vector3 vec = new Vector3();
                     vec = inventory.slots[i].transform.position;
                     var obj = Instantiate(itemButton, canvas.transform);
-                    Debug.Log("I instantiated the itemButton");
                     obj.transform.SetParent(inventory.slots[i].transform);
-                    //var obj = Instantiate(itemButton, inventory.slots[i].transform,false);
                     obj.transform.position = vec;
                     GameObject currentSlot = findSlot(i,other.gameObject);
                     currentSlot.GetComponent<Slot>().inventoryObject = gameObject;
@@ -58,14 +54,14 @@ public class Pickup : MonoBehaviour
     {
         Transform trans = GameObject.Find("brother's inventory").transform;
         Transform childTrans = trans.Find("Slot (" + (i+1).ToString() + ")");
-        if(childTrans != null)
-        {
-            Debug.Log("current slot is " + childTrans.gameObject);
-        }
-        else
-        {
-            Debug.Log("no children found");
-        }
+        //if(childTrans != null)
+        //{
+        //    Debug.Log("current slot is " + childTrans.gameObject);
+        //}
+        //else
+        //{
+        //    Debug.Log("no children found");
+        //}
         return childTrans.gameObject;
     }
 
--- pc5game/Assets/Scripts/Slot.cs
+++ pc5game/Assets/Scripts/Slot.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class Slot : MonoBehaviour
 {
@@ -8,13 +9,20 @@ public class Slot : MonoBehaviour
     public GameObject inventoryObject;
     public void DropItem()
     {
-        Debug.Log(transform.gameObject);
         foreach (Transform child in transform)
         {
             Debug.Log("destoying the child object " + child.gameObject.name);
+            resetSlot(transform.gameObject.name); //resets slot so it's reusable
             inventoryObject.SetActive(true);
             Destroy(child.gameObject);
 
+
         }
     }
+    public void resetSlot(string slotName)
+    {
+        int indexValue = int.Parse(Regex.Match(slotName, @"\d+").Value);
+        inventoryObject.transform.GetComponent<Pickup>().inventory.isFull[indexValue-1] = false;
+    }
+
 }
