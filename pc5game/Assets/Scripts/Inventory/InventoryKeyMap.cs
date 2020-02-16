fd2be62724e4f4483ea3d6d3345e578c8266492f
Jamalzadeh, Mojan <mojan.jamalzadeh@philips.com>
2020-02-16 14:48:43 -0800

    started on the sister's inventory

---
+++ pc5game/Assets/Scripts/Inventory/InventoryKeyMap.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryKeyMap : MonoBehaviour
+{
+    public KeyCode RightCell = KeyCode.X;
+    public KeyCode LeftCell = KeyCode.Z;
+    public KeyCode UseObject = KeyCode.C;
+}
