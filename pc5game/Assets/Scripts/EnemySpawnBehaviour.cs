792bbb7de807747983f7db5cc0fd9bca26b97bb7
Dennis Truong <dennisnhatsontruong@gmail.com>
2020-03-26 15:59:27 -0700

    added 3 spawn points that uses a timer

---
+++ pc5game/Assets/Scripts/EnemySpawnBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnBehaviour : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float respawnTime = 2.0f;
+    public Vector2[] spawnPoints;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(enemyWave());
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab) as GameObject;
+        int positionSelector = Random.Range(0, spawnPoints.Length);
+        enemy.transform.position = spawnPoints[positionSelector];
+    }
+
+    IEnumerator enemyWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnEnemy();
+        }
+
+    }
+}
